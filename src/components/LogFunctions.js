6ced78d2a42c2c2b5787f707b29830d211ec1680
mdnelles <mxnelles@gmail.com>
2021-02-09 18:47:40 -0500

    shorthand corrections

--- src/components/LogFunctions.js
+++ src/components/LogFunctions.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
-import './config'; // adding config for folder specific build
+import axios from "axios";
+import "./config"; // adding config for folder specific build
 
 var thisServer = window.location.href;
 var serverPath = global.config.routerPath;
-if (thisServer.includes('3000')) serverPath = global.config.devPath;
+if (thisServer.includes("3000")) serverPath = global.config.devPath;
 
 ////// playlists
-export const getLogs = async (theToken) => {
-   console.log('getLogs');
+export const getLogs = async (token) => {
+   console.log("getLogs");
    try {
-      const res = await axios.post(serverPath + '/logs/get_logs', {
-         token: theToken,
-         caller: 'logFunctions.getLogs',
+      const res = await axios.post(serverPath + "/logs/get_logs", {
+         token,
+         caller: "logFunctions.getLogs",
       });
       return res.data;
    } catch (err) {
-      console.log('ClientSide Error @ logFunctions.getLogs' + err);
+      console.log("ClientSide Error @ logFunctions.getLogs" + err);
       return false;
    }
 };
 
-export const getLogsCount = async (theToken, code) => {
+export const getLogsCount = async (token, code) => {
    try {
-      const res = await axios.post(serverPath + '/logs/get_logcount', {
-         token: theToken,
+      const res = await axios.post(serverPath + "/logs/get_logcount", {
+         token,
          code,
-         caller: 'logFunctions.getLogsCount',
+         caller: "logFunctions.getLogsCount",
       });
       return res.data;
    } catch (err) {
-      console.log('ClientSide Error @ logFunctions.getLogsCount' + err);
+      console.log("ClientSide Error @ logFunctions.getLogsCount" + err);
       return false;
    }
 };
